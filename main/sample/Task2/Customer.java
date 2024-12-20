2e63bac075a275285f18e8654ba7b8f117ac709d
GiangNguyenn <giang.nguyen@skedulo.com>
2024-12-20 23:48:35 +0700

    add task 2

---
+++ main/sample/Task2/Customer.java
@@ -0,0 +1,119 @@
+package sample.Task2;
+
+
+public class Customer {
+    private String name;
+    private String email;
+    private int customerId;
+    private Privilege privilege;
+
+    public Customer(String inName, int inCustomerId, String inEmail, Privilege inPrivilege) {
+        setName(inName);
+
+        setCustomerId(inCustomerId);
+
+        setEmail(inEmail);
+
+        setPrivilege(inPrivilege);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public Privilege getPrivilege() {
+        return privilege;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setName(String inName) {
+        if (isValidName(inName)) {
+            name = inName;
+        } else {
+            name = "ERROR";
+        }
+    }
+
+
+    public void setPrivilege(Privilege inPrivilege) {
+        privilege = inPrivilege;
+    }
+
+    public void setEmail(String inEmail) {
+        if (isValidEmail(inEmail)) {
+            email = inEmail;
+        } else {
+            email = "ERROR";
+        }
+    }
+
+    private boolean isValidEmail(String inEmail) {
+        // check if email exceed 125
+        if(inEmail.length() > 125){
+            return false;
+        }
+
+        if(inEmail.split("@").length > 2){
+            return false;
+        }
+
+        if(!inEmail.split("\\.")[1].equals(".com") || inEmail.split("\\.")[1].equals(".vn")){
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isValidName(String name) {
+        // Check if the name contains at least 2 characters
+        if (name.length() < 2) {
+            return false;
+        }
+
+        // Check for leading or trailing spaces
+        if (name.trim().length() != name.length()) {
+            return false;
+        }
+
+        // Check if the name contains only alphabet characters (title case)
+        if (!name.matches("^[A-Z][a-z]+( [A-Z][a-z]+)*$")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isValidCustomerId(int inCustomerId) {
+        if (inCustomerId < 0) {
+            return false;
+        }
+
+        for(int i = 0; i < String.valueOf(inCustomerId).length(); i++) {
+            if (!Character.isDigit(String.valueOf(inCustomerId).charAt(i))) {
+                return false;
+            }
+        }
+
+        if (String.valueOf(inCustomerId).length() != 8) {
+            return false;
+        }
+        return true;
+    }
+
+    public void setCustomerId(int inCustomerId) {
+        if (isValidCustomerId(inCustomerId)) {
+            customerId = inCustomerId;
+        }
+        else{
+            customerId = 0;
+        }
+    }
+}
---
+++ main/sample/Task2/Privilege.java
@@ -0,0 +1,24 @@
+package sample.Task2;
+
+public enum Privilege {
+    GUEST,
+    SILVER,
+    GOLD,
+    DIAMOND;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case GUEST:
+                return "Guest";
+            case SILVER:
+                return "Silver";
+            case GOLD:
+                return "Gold";
+            case DIAMOND:
+                return "Diamond";
+            default:
+                return "ERROR";
+        }
+    }
+}
