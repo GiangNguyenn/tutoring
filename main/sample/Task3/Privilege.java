74673e0366fa292d0744b2a0f7a687a4d968c826
GiangNguyenn <giang.nguyen@skedulo.com>
2024-12-21 10:04:59 +0700

    modify task 3 enum

--- main/sample/Task3/Privilege.java
+++ main/sample/Task3/Privilege.java
@@ -1,24 +1,74 @@
 package sample.Task3;
 
 public enum Privilege {
-    GUEST,
-    SILVER,
-    GOLD,
-    DIAMOND;
-
-    @Override
-    public String toString() {
-        switch (this) {
-            case GUEST:
-                return "Guest";
-            case SILVER:
-                return "Silver";
-            case GOLD:
-                return "Gold";
-            case DIAMOND:
-                return "Diamond";
-            default:
-                return "ERROR";
+    GUEST {
+        @Override
+        public String toString() {
+            return "Guest";
         }
-    }
+    },
+    SILVER {
+        @Override
+        public String toString() {
+            return "Silver";
+        }
+    },
+    GOLD {
+        @Override
+        public String toString() {
+            return "Gold";
+        }
+    },
+    DIAMOND {
+        @Override
+        public String toString() {
+            return "Diamond";
+        }
+    };
 }
+/**
+ * public enum State {
+ * NEW_SOUTH_WALES{
+ *
+ * @Override public String toString() {
+ * return "New South Wales";
+ * }
+ * },
+ * AUSTRALIAN_CAPITAL_TERRITORY{
+ * @Override public String toString() {
+ * return "Australian Capital Territory";
+ * }
+ * },
+ * VICTORIA{
+ * @Override public String toString() {
+ * return "Victoria";
+ * }
+ * },
+ * QUEENSLAND{
+ * @Override public String toString() {
+ * return "Queensland";
+ * }
+ * },
+ * SOUTH_AUSTRALIA{
+ * @Override public String toString() {
+ * return "South Australia";
+ * }
+ * },
+ * WESTERN_AUSTRALIA{
+ * @Override public String toString() {
+ * return "Western Australia";
+ * }
+ * },
+ * TASMANIA{
+ * @Override public String toString() {
+ * return "Tasmania";
+ * }
+ * },
+ * NORTHERN_TERRITORY{
+ * @Override public String toString() {
+ * return "Northern Territory";
+ * }
+ * },
+ * <p>
+ * }
+ */
